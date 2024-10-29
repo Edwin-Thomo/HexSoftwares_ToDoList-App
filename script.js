3a96b0899506ba4979406d2b1b6d542d4a70932e
Edwin Thomo <55330741+EdLocalman@users.noreply.github.com>
2024-10-29 09:38:10 +0200

    Create script.js
    This is the script for the  application

---
+++ script.js
@@ -0,0 +1,132 @@
+const inputBox = document.getElementById('inputBox');
+const addBtn = document.getElementById('addBtn');
+const todoList = document.getElementById('todoList'); 
+
+let editTodo = null;
+
+//Adding To Do
+const addTodo = ()=>{
+    const inputText = inputBox.value.trim();
+    if(inputText.length <=0){
+        alert("You must write something in your to do");
+        return false;
+    }
+
+    if(addBtn.value ==="Edit"){
+        editLocalTodos(editTodo.target.previousElementSibling.innerHTML);
+        editTodo.target.previousElementSibling.innerHTML =inputText;
+        editLocalTodos(inputText);
+        addBtn.value="Add";
+        inputBox.value ="";
+    }
+    else{
+
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerHTML =inputText;
+    li.appendChild(p);
+
+    //Edit Button Btn
+    const editBtn =document.createElement("button");
+    editBtn.innerText ="Edit";
+    editBtn.classList.add("btn","editBtn");
+    li.appendChild(editBtn);
+
+    //Delete Button Btn
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText ="Remove";
+    deleteBtn.classList.add("btn","deleteBtn");
+    li.appendChild(deleteBtn);
+
+     
+    todoList.appendChild(li);
+    inputBox.value = "";
+
+    saveLocalTodos(inputText);
+}
+}
+//Updating To Do(Edit/Delete functions)
+const updateToDo =(e)=>{
+  
+    if(e.target.innerHTML ==="Remove"){
+       todoList.removeChild(e.target.parentElement);
+        deleteLocalTodos(e.target.parentElement);
+    }
+    if(e.target.innerHTML ==="Edit"){
+       inputBox.value =e.target.previousElementSibling.innerHTML;
+       inputBox.focus();
+       addBtn.value ="Edit";
+       editTodo = e;
+    }
+}
+//Save ToDo
+const saveLocalTodos= (todo)=>{
+      let todos = [];
+      if(localStorage.getItem("todos") === null){
+        todos = [];
+      }
+      else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+      }
+      todos.push(todo);
+      localStorage.setItem("todos",JSON.stringify(todos));
+    
+
+      
+}
+const getLocalTodos = ()=>{
+    let todos;
+      if(localStorage.getItem("todos") === null){
+        todos = [];
+      }
+      else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+        todos.forEach(todo => {
+
+            const li = document.createElement("li");
+            const p = document.createElement("p");
+            p.innerHTML = todo;
+            li.appendChild(p);
+        
+            //Edit Button Btn
+            const editBtn =document.createElement("button");
+            editBtn.innerText ="Edit";
+            editBtn.classList.add("btn","editBtn");
+            li.appendChild(editBtn);
+        
+            //Delete Button Btn
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerText ="Remove";
+            deleteBtn.classList.add("btn","deleteBtn");
+            li.appendChild(deleteBtn);
+        
+             
+            todoList.appendChild(li);
+        });
+      }
+}
+//Delete ToDo
+const deleteLocalTodos =(todo)=>{
+      let todos;
+      if(localStorage.getItem("todos")===null){
+        todos =[];
+      }
+      else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+      }
+      let todoText = todo.children[0].innerHTML;
+      let todoIndex = todos.indexOf(todoText);
+      todos.splice(todoIndex,1);
+      localStorage.setItem("todos",JSON.stringify(todos));
+    console.log(todoIndex);
+    
+}
+const editLocalTodos =(todo)=>{
+     let todos = JSON.parse(localStorage.getItem("todos"));
+     let todoIndex = todos.indexOf(todo);
+     todos[todoIndex] = inputBox.value;
+     localStorage.setItem("todos",JSON.stringify(todos));    
+}
+document.addEventListener('DOMContentLoaded',getLocalTodos);
+addBtn.addEventListener('click',addTodo);
+todoList.addEventListener('click',updateToDo);
